1fdd47542b041a6b2b686791d44a421e3acd739d
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-07-25 14:21:38 -0700

    Implemented GetAllBooks and get all Customers

---
+++ BookRegistrationEF/BookDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF
+{
+    static class BookDB
+    {
+        public static List<Book> GetAllBooks()
+        {
+            var context = new BookContext();
+            List<Book> allBooks =
+                (from b in context.Book
+                 select b).ToList();
+            return allBooks;
+        }
+    }
+}
---
+++ BookRegistrationEF/CustomerDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF
+{
+    static class CustomerDB
+    {
+        public static List<Customer> GetAllCustomers()
+        {
+            BookContext context = new BookContext();
+            List<Customer> customers = 
+                context.Customer.ToList();
+
+            return customers;
+        }
+    }
+}
